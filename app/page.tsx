29afd94511255a33d40fcc82ca7b4d96db919c49
Najmuddin ansari <najmuddin.ansari@appinventiv.com>
2025-11-08 13:46:25 +0530

    Changes in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1 className="text-red">hello world najmuddin</h1>
+    <h1 className="text-red">hello world najmuddin ansari</h1>
     </>
   );
 }
